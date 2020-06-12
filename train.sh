2f3a46da913562750150f92bfc07c708a828fe69
Michael Zeng <michael.zengzf@outlook.com>
2020-06-12 10:18:46 -0700

    remove unused

--- train.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-nvidia-smi
-conda env create -f environment.yml
-conda activate zb
-pip install Pillow==6.2.2
-python main.py
--- upload.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-kubectl cp ./download.sh xyang-login:Text2image/code/code/download.sh
